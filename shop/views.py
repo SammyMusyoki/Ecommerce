424d843f2359f23d40212da11e5f79b5dcb500c3
sammy musyoki <75688249+SammyMusyoki@users.noreply.github.com>
2023-03-26 02:35:00 +0300

    product-add-temp

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .models import Category, Products, Shop, Order
 
 # Create your views here.
 
 def home(request):
+    products = Products.objects.all()
+    category = Category.objects.all()
 
     context = {
-        
+        'products' : products,
+        'category': category
+
     }
     return render(request, 'shop/home.html', context)
